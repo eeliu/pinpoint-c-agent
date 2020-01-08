5fb5311725f61c318ff0deb06e62c0145c07e450
liumingyi <liumingyi@navercorp.com>
2020-01-08 16:28:57 +0800

    unittest pass

---
+++ common/test/test_main.cpp
@@ -0,0 +1,8 @@
+﻿#include <gtest/gtest.h>
+#include "common.h"
+
+int main(int argc, char **argv) {
+    register_error_cb(NULL);
+    ::testing::InitGoogleTest(&argc, argv); 
+    return RUN_ALL_TESTS();
+}
