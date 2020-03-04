1b9e8abd97235ac57f02af14cc1450d83c322f64
liumingyi <liumingyi@navercorp.com>
2020-03-04 18:13:25 +0800

    pinpoint wrapper

---
+++ PY/pinpoint-py-example/PinpointCommon.py
@@ -0,0 +1,41 @@
+﻿import pinpoint
+
+PYTHON = 1700
+PYTHON_METHOD_CALL=1701
+PYTHON_REMOTE_METHOD = 9900
+
+
+
+
+class Candy(object):
+    def onBefore(self,*args, **kwargs):
+        pinpoint.start_trace()
+
+    def onEnd(self,ret):
+        pinpoint.end_trace()
+        return ret
+
+    def onException(self,e):
+        pass
+
+    def __call__(self, func):
+        def pinpointTrace(*args, **kwargs):
+            ret = None
+            try:
+                self.onBefore(*args, **kwargs)
+                ret = func(*args, **kwargs)
+            except Exception as e:
+                self.onException(e)
+                ret = None
+            finally:
+                self.onEnd(ret)
+        return pinpointTrace
+        
+
+class BaseHTTPRequestPlugins(object):
+
+    def onBefore(self):
+        pass
+
+    def onEnd(self):
+        pass
