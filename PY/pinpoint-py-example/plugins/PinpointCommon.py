4094dffb2e66d6b72fcc90113fc167e4bc567964
eeliu <eeliu2009@gmail.com>
2020-03-10 16:36:00 +0800

    Add license header

--- PY/pinpoint-py-example/plugins/PinpointCommon.py
+++ PY/pinpoint-py-example/plugins/PinpointCommon.py
@@ -1,4 +1,24 @@
-﻿import pinpoint
+﻿#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Created by eeliu at 3/5/20
+
+# ------------------------------------------------------------------------------
+#  Copyright  2020. NAVER Corp.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# ------------------------------------------------------------------------------
+
+import pinpoint
 import random
 
 PYTHON = '1700'
@@ -79,6 +99,7 @@ class Candy(object):
                 print(e)
             finally:
                 return self.onEnd(ret)
+        return pinpointTrace
 
     def generateTid(self):
         return ('%s^%s^%s') % (APP_ID,str(pinpoint.start_time()), str(pinpoint.unique_id()))
@@ -94,7 +115,7 @@ class Candy(object):
 
 if __name__ == '__main__':
 
-    @Candy()
+    @Candy('main',__name__)
     def run():
         print("run")
 
