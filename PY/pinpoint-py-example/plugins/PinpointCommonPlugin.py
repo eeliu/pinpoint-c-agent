bcc66eb5cb711b7b9c5c45c0ec905bec70230a5a
eeliu <eeliu2009@gmail.com>
2020-03-10 16:36:00 +0800

    Add license header

---
+++ PY/pinpoint-py-example/plugins/PinpointCommonPlugin.py
@@ -0,0 +1,23 @@
+﻿from plugins.PinpointCommon import *
+import pinpoint
+
+class PinpointCommonPlugin(Candy):
+
+    def onBefore(self,*args, **kwargs):
+        super().onBefore(*args, **kwargs)
+        ###############################################################
+        pinpoint.add_clue(FuncName,self.getFuncUniqueName())
+        pinpoint.add_clue(ServerType,PYTHON_METHOD_CALL);
+        pinpoint.add_clues(PY_ARGS,'special your input values')
+        ###############################################################
+
+    def onEnd(self,ret):
+        ###############################################################
+        pinpoint.add_clues(PY_RETURN,str(ret))
+        ###############################################################
+        super().onEnd(ret)
+        return ret
+
+    def onException(self, e):
+        pinpoint.add_clue('EXP',e)
+        raise e
